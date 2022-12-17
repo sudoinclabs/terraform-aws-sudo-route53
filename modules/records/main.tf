9a192a400e1e8f233579e32a923ba635d5aeacc0
Hameedullah Khan <hameed@sudoconsultants.com>
2022-12-17 21:54:51 +0400

    Update the name to be fqdn

--- modules/records/main.tf
+++ modules/records/main.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "this" {
 
   allow_overwrite = true
   zone_id         = var.zone_id
-  name            = each.key == "@" ? var.zone_name : each.key
+  name            = each.key == "@" ? var.zone_name : "${each.key}.${var.zone_name}"
   type            = lower(var.type) != "alias" ? upper(var.type) : "A"
   records         = lower(var.type) != "alias" ? each.value.value : null
   ttl             = lower(var.type) != "alias" ? lookup(each.value, "ttl", var.ttl) : null
