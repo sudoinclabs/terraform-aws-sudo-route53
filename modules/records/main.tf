beadfc3860a6752c33f161776def8cdecd9e17f0
Shahzaibkha9 <sk0141361@gmail.com>
2024-09-02 18:13:33 +0500

    Update DNS record logic

--- modules/records/main.tf
+++ modules/records/main.tf
@@ -7,10 +7,11 @@ resource "aws_route53_record" "this" {
 
   allow_overwrite = true
   zone_id         = var.zone_id
-  name            = each.key == "@" ? var.zone_name : "${each.key}.${var.zone_name}"
-  type            = lower(var.type) != "alias" ? upper(var.type) : "A"
-  records         = lower(var.type) != "alias" ? each.value.value : null
-  ttl             = lower(var.type) != "alias" ? lookup(each.value, "ttl", var.ttl) : null
+  name            = each.key == "@" ? var.zone_name : (endswith(each.key, var.zone_name) ? each.key : "${each.key}.${var.zone_name}")
+  # name            = each.key == "@" ? var.zone_name : "${each.key}.${var.zone_name}"
+  type    = lower(var.type) != "alias" ? upper(var.type) : "A"
+  records = lower(var.type) != "alias" ? each.value.value : null
+  ttl     = lower(var.type) != "alias" ? lookup(each.value, "ttl", var.ttl) : null
 
   dynamic "alias" {
     for_each = length(keys(lookup(each.value, "alias", {}))) == 0 ? [] : [true]
